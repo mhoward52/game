113ec31eca2e7af1c6a507ada972d64e3c258dba
Michael Howard <michael.howard.pha@icloud.com>
2019-04-03 16:47:33 -0500

    Added enemy spawns

--- dist/js/main.js
+++ dist/js/main.js
@@ -22,28 +22,82 @@ var game = (function () {
     y: 0,
     h: 10,
     w: 10,
-    fill: '#ff0',
+    fill: '#9997ef',
     speed: 5
   }
+
+  var spawns = {}
+
+  var spawner = null;
 
   //2. Create a method for launching spawns
   // this iteration will launch a single spawn
   function launchSpawns() {
-    ctx.fillStyle = spawn.fill;
+    //3. Create a new enemy spawn every 400 ms
+    spawner = setInterval(() => {
+      //4. Use psuedo-random strings to name the new spawns
+      var text = "";
+      var possible = "abcdefghijklmnopqrstuvwxyz";
 
-    ctx.clearRect(
-      spawn.x - 1,
-      spawn.y - spawn.speed,
-      spawn.w + 2,
-      spawn.h + 2
-    );
+      for (var i = 0; i < 10; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+      }
 
-    ctx.fillRect(
-      spawn.x,
-      spawn.y = (spawn.y + spawn.speed),
-      spawn.w,
-      spawn.h
-    );
+      //5. Add the new spawn to the Object of Spawns
+      spawns[text] = {
+        x: Math.floor(Math.random() * this.canvas.width),
+        y: spawn.y,
+        h: spawn.h,
+        w: spawn.w,
+        fill: spawn.fill,
+        speed: spawn.speed,
+      }
+
+    }, 1000);
+  }
+
+  //6. Move all spawns
+  function moveSpawns() {
+
+    //7. Loop through the Object of spawns
+    //and move each one individually.
+    //This will look a lot like movePlayer()
+    if (Object.keys(spawns).length > 0) {
+      for (let spawn in spawns) {
+
+        //8. Only move the spawn, if the spawn has not 
+        //moved off of the screen.
+        if (spawns[spawn].y <= canvas.height) {
+
+          ctx.fillStyle = spawns[spawn].fill;
+
+          ctx.save();
+
+          ctx.clearRect(
+            spawns[spawn].x - 1,
+            spawns[spawn].y - spawns[spawn].speed,
+            spawns[spawn].w + 2,
+            spawns[spawn].h + 2
+          );
+
+          ctx.fillRect(
+            spawns[spawn].x,
+            spawns[spawn].y = (spawns[spawn].y + spawns[spawn].speed),
+            spawns[spawn].w,
+            spawns[spawn].h
+          );
+
+          ctx.restore();
+
+
+        } else {
+          //9. Delete the spawn from the Object of spawns if 
+          // that spawn has moved off of the screen.
+          delete spawns[spawn];
+        }
+      }
+    }
+
   }
 
   return {
@@ -54,17 +108,18 @@ var game = (function () {
 
       //1. Define how many pixels the player
       // should move each frame (i.e. speed)
-      //2. Change x-1 to player.x-player.speed
-      ctx.clearRect(
-        player.x - player.speed,
-        player.y - 1,
-        player.w + 2,
-        player.h + 2
-      );
+
 
       //2. Add x pixels to move the player to the right
       if (player.dir === 'right') {
         //3. Change player.x++ to player.x = (player.x + player.speed)
+        //2. Change x-1 to player.x-player.speed
+        ctx.clearRect(
+          player.x - player.speed,
+          player.y - 1,
+          player.w + 2,
+          player.h + 2
+        );
         ctx.fillRect(
           player.x = (player.x + player.speed),
           player.y,
@@ -117,23 +172,19 @@ var game = (function () {
       canvas.height = 600;
       canvas.width = 800;
 
-      this.player();
+      launchSpawns();
+      this.animate();
+
     },
 
     //2. Create an animation frame
     //3. Redraw the player every time a frame is executed
     animate: function () {
       this.player();
-      launchSpawns();
+      moveSpawns();
       window.requestAnimationFrame(this.animate.bind(this));
-    },
-
-    init: function () {
-      canvas.height = 600;
-      canvas.width = 800;
-
-      this.animate();
     }
+
   }
 })();
 
